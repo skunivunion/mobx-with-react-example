eadda9ec629b88529c17eb46084136556f11f6ff
Seungman <smjang67@gmail.com>
2020-04-30 21:10:01 +0900

    mobx 기본

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,31 +1,51 @@
 import React, { Component } from 'react';
-import { decorate, observable, action } from 'mobx';
-import { observer } from 'mobx-react';
+// import { decorate, observable, action } from 'mobx';
+// inject 함수는 mobx-react 에 있는 함수로서, 컴포넌트에서 스토어에 접근할 수 있게 해준다.
+// 정확히는, 스토어에 있는 값을 컴포넌트의 props 로 "주입" 해준다
+import { observer, inject } from 'mobx-react';
 
+// inject('스토어이름') 을 하시면
+// 컴포넌트에서 해당 스토어를 props 로 전달받아서 사용 할 수 있다.
+@inject('counterStore')
+// // 스토어의 특정 값이나 함수만 넣어주고 싶다면...
+// @inject(stores => ({
+//     number: stores.counterStore.number,
+//     increase: stores.counterStore.increase,
+//     decrease: stores.counterStore.decrease,
+// }))
+@observer
 class Counter extends Component {
-    @observable
-    number = 0;
+    // @observable
+    // number = 0;
 
-    @action
-    increase = () => {
-        this.number++;
-    }
+    // @action
+    // increase = () => {
+    //     this.number++;
+    // }
 
-    @action
-    decrease = () => {
-        this.number--;
-    }
+    // @action
+    // decrease = () => {
+    //     this.number--;
+    // }
 
     render() {
+        const { counterStore } = this.props;
+        // // 스토어의 특정 값이나 함수만 넣어주고 싶다면...
+        // const { number, increase, decrease } = this.props;
         return (
             <div>
-                <h1>{this.number}</h1>
-                <button onClick={this.increase}>+1</button>
-                <button onClick={this.decrease}>-1</button>
+                <h1>{counterStore.number}</h1>
+                <button onClick={counterStore.increase}>+1</button>
+                <button onClick={counterStore.decrease}>-1</button>
+                {/* 스토어의 특정 값이나 함수만 넣어주고 싶다면...
+                <button onClick={increase}>+1</button>
+                <button onClick={decrease}>-1</button> */}
             </div>
-        )
+        );
     }
 }
+
+export default Counter; // @observer 데코레이션 사용
 
 // decorate(Counter, {
 //     number: observable,
@@ -33,6 +53,6 @@ class Counter extends Component {
 //     decrease: action
 // });
 
-// observer가 observable 값이 변할 때
+// observable 값이 변할 때 observer가 
 // 컴포넌트의 forceUpdate 를 호출하게 함으로써 자동으로 변화가 화면에 반영
-export default observer(Counter);
+//export default observer(Counter);
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'mobx-react';
+import CounterStore from './stores/CounterStore';
+
+// 스토어 인스턴스 생성
+const counterStore = new CounterStore();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider counterStore={counterStore}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
